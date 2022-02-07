9bd1e557b2c839dea4c247e27e5d113473c4033f
Alan Rauzier <alan.rauzier@gmail.com>
2022-02-07 11:25:49 +0100

    v1.0.3

--- build/lib/flask_utils/json.py
+++ build/lib/flask_utils/json.py
@@ -3,3 +3,12 @@ import json
 
 def jsonify(self):
     return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
+def jsonify_model(model, keys):
+    object_hash = {}
+    for key in keys:
+        object_hash[key] = getattr(model, key)
+    return json.dumps(
+        object_hash, default=lambda o: o.__dict__, sort_keys=True, indent=4
+    )
