6d164b08c6ae6f5df7def47deeb69020d0e35b15
Alan Rauzier <alan.rauzier@gmail.com>
2022-02-07 11:31:33 +0100

    V1.0.6

--- build/lib/flask_utils/json.py
+++ build/lib/flask_utils/json.py
@@ -1,8 +1,16 @@
 import json
+import datetime
+
+
+def json_default(value):
+    if isinstance(value, datetime.date):
+        return dict(year=value.year, month=value.month, day=value.day)
+    else:
+        return value.__dict__
 
 
 def jsonify(self):
-    return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+    return json.dumps(self, default=lambda o: json_default(o), sort_keys=True, indent=4)
 
 
 def jsonify_model(model, keys):
@@ -10,5 +18,5 @@ def jsonify_model(model, keys):
     for key in keys:
         object_hash[key] = getattr(model, key)
     return json.dumps(
-        object_hash, default=lambda o: o.__dict__, sort_keys=True, indent=4
+        object_hash, default=lambda o: json_default(o), sort_keys=True, indent=4
     )
