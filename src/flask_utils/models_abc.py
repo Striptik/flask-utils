ac0096a54d222ae338be80bd0d073dfc0dd857c8
Alan Rauzier <alan.rauzier@gmail.com>
2022-02-14 10:01:47 +0100

    Fix model abc

--- src/flask_utils/models_abc.py
+++ src/flask_utils/models_abc.py
@@ -5,9 +5,7 @@ import json
 from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import inspect
 from sqlalchemy.orm import aliased
-from sqlalchemy.orm.collections import CollectionAdapter
 from weakref import WeakValueDictionary
 
 db = SQLAlchemy()
@@ -30,76 +28,29 @@ class MetaBaseModel(db.Model.__class__):
         return alias
 
 
-def get_value(value):
-    try:
-        return getattr(value, "___dict___", {})
-    except Exception:
-        return value if not isinstance(value, datetime) else value.strftime("%d/%m/%Y")
-
-
-def json_default(value):
-    if isinstance(value, datetime):
-        return dict(year=value.year, month=value.month, day=value.day)
-    elif isinstance(value, CollectionAdapter):
-        return []
-    else:
-        return getattr(value, "___dict___", {})
-
-
 class BaseModel:
-    """Generalize __init__, __repr__ and to_json
-    Based on the models columns"""
-
-    print_filter = ()
-    to_json_filter = ()
 
     def __repr__(self):
-        """Define a base way to print models
-        Columns inside `print_filter` are excluded"""
-        return "%s(%s)" % (
-            self.__class__.__name__,
-            {
-                column: value.__dict__
-                for column, value in self._to_dict().items()
-                if column not in self.print_filter
-            },
-        )
+        return self.repr()
 
     def __str__(self):
-        object_hash = {
-            column: get_value(value)
-            for column, value in self._to_dict().items()
-            if column not in self.print_filter
-        }
-        return "%s(%s)" % (
-            self.__class__.__name__,
-            json.dumps(
-                object_hash, default=lambda o: json_default(o), sort_keys=True, indent=4
-            ),
-        )
+        return self.repr()
 
-    @property
-    def json(self):
-        """Define a base way to jsonify models
-        Columns inside `to_json_filter` are excluded"""
+    def repr(self):
+        return f"{self.__class__.__name__}({json.dumps(self.tojson(), indent=4, sort_keys=True)})"
+
+    def tojson(self):
         return {
             column: value
             if not isinstance(value, datetime)
             else value.strftime("%Y-%m-%d")
-            for column, value in self._to_dict().items()
-            if column not in self.to_json_filter
+            for column, value in self.todict().items()
         }
 
-    def _to_dict(self):
-        """This would more or less be the same as a `to_json`
-        But putting it in a "private" function
-        Allows to_json to be overriden without impacting __repr__
-        Or the other way around
-        And to add filter lists"""
-        return {
-            column.key: getattr(self, column.key)
-            for column in inspect(self.__class__).attrs
-        }
+    def todict(self):
+        excl = ('_sa_adapter', '_sa_instance_state')
+        return {k: v for k, v in vars(self).items() if not k.startswith('_') and
+                not any(hasattr(v, a) for a in excl)}
 
     def save(self):
         db.session.add(self)
