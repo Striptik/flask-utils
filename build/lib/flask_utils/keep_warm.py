537718b0d3946e4d3f63847ffd38f6d1391ebdfb
Alan Rauzier <alan.rauzier@gmail.com>
2022-04-22 17:20:50 +0200

    Keep warm

--- build/lib/flask_utils/keep_warm.py
+++ build/lib/flask_utils/keep_warm.py
@@ -1,12 +1,14 @@
 import os
+
 import time
-from flask_utils import log_info
 from zappa.asynchronous import task
+
+from flask_utils import log_info
 
 
 @task
 def keep_warm_task(lambda_count):
     log_info(f"Lambda keep warm {lambda_count}")
-    if lambda_count < os.getenv("KEEP_WARM_LAMBDAS", ""):
+    if lambda_count < os.getenv("KEEP_WARM_LAMBDAS", 1):
         keep_warm_task(lambda_count + 1)
         time.sleep(3)
--- src/flask_utils/keep_warm.py
+++ src/flask_utils/keep_warm.py
@@ -9,6 +9,6 @@ from flask_utils import log_info
 @task
 def keep_warm_task(lambda_count):
     log_info(f"Lambda keep warm {lambda_count}")
-    if lambda_count < os.getenv("KEEP_WARM_LAMBDAS", ""):
+    if lambda_count < os.getenv("KEEP_WARM_LAMBDAS", 1):
         keep_warm_task(lambda_count + 1)
         time.sleep(3)
