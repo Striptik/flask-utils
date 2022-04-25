831714698b042bb051ea66f0cb1707ebb1a18fb1
Alan Rauzier <alan.rauzier@gmail.com>
2022-04-25 12:38:37 +0200

    Fix log

--- src/flask_utils/s3.py
+++ src/flask_utils/s3.py
@@ -12,7 +12,7 @@ def s3_client(s3_key, s3_secret):
         aws_secret_access_key=s3_secret,
         region_name="eu-west-1",
     )
-    client = session.client("s3", config=Config(connect_timeout=5, read_timeout=10, retries={'max_attempts': 2}))
+    client = session.client("s3", config=Config(connect_timeout=5, read_timeout=5, retries={'max_attempts': 1}))
     return client
 
 
