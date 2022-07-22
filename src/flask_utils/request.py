74ffca47b488854f430546f766eb7405f48ddd76
Alan Rauzier <alan.rauzier@gmail.com>
2022-07-22 16:23:47 +0200

    Add request helper

--- src/flask_utils/request.py
+++ src/flask_utils/request.py
@@ -3,9 +3,9 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
 
-def custom_request(url, method, headers=None, json=None, timeout=(5, 20)):
+def custom_request(url, method, headers=None, json=None, timeout=(5, 20), connect_retry=3):
     session = requests.Session()
-    retry = Retry(connect=3, backoff_factor=0.5)
+    retry = Retry(connect=connect_retry, backoff_factor=0.5)
     adapter = HTTPAdapter(max_retries=retry)
     session.mount("https://", adapter)
     session.mount("http://", adapter)
